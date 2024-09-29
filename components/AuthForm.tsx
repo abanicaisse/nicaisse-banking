1ceb1ac2897bc299f73a586d092f64138709ed3b
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-09-29 16:53:19 +0300

    bug: implement PlaidLink and Plaid, still bug to be fixed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,6 +24,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { redirect, useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.action";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -58,9 +59,21 @@ const AuthForm = ({ type }: { type: string }) => {
     console.log(data);
 
     try {
-      // TODO: Sign Up with Appwrite & Create a Plaid Link Token
+      // TODO: Create a Plaid Link Token
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
 
@@ -103,7 +116,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* Plaid Link  */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
